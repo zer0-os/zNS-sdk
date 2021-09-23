0de490919140783f306febaca4cb96b3eff2f662
Zachary <remscar@gmail.com>
2021-09-23 16:44:25 -0700

    More fixes

--- src/api/actions/helpers.ts
+++ src/api/actions/helpers.ts
@@ -5,16 +5,24 @@ export const makeApiCall = async <T>(
   method: "GET" | "POST",
   body?: string | Record<string, unknown> | Buffer
 ): Promise<T> => {
+  const headers: Record<string, string> = {};
+
   if (body) {
     if (typeof body !== "string" && !Buffer.isBuffer(body)) {
       body = JSON.stringify(body);
+      headers["Content-Type"] = "application/json";
     }
   }
 
   const res = await fetch(url, {
     method,
     body,
+    headers,
   });
+
+  if (res.status !== 200) {
+    throw Error(`Request failed with code ${res.status}: ${await res.text()}`);
+  }
 
   const returnedBody = await res.json();
   return returnedBody as T;
