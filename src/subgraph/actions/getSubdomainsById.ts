93c75db828a8bd5c0ec98fc9e2d609511a39aa60
Zachary <remscar@gmail.com>
2021-10-11 11:11:10 -0700

    Queries addtional times if there are more than 1000 subdomains to return a full list

--- src/subgraph/actions/getSubdomainsById.ts
+++ src/subgraph/actions/getSubdomainsById.ts
@@ -8,13 +8,33 @@ export const getSubdomainsById = async <T>(
   apolloClient: ApolloClient<T>,
   domainId: string
 ): Promise<Domain[]> => {
-  const queryResult = await performQuery<DomainsQueryDto>(
-    apolloClient,
-    queries.getSubdomainsById,
-    { parent: domainId }
-  );
+  const queryCount = 1000;
+  let skip = 0;
 
-  const queriedDomains = queryResult.data.domains;
-  const domains: Domain[] = queriedDomains.map(convertDomainDtoToDomain);
+  const domains: Domain[] = [];
+
+  while (true) {
+    const queryResult = await performQuery<DomainsQueryDto>(
+      apolloClient,
+      queries.getSubdomainsById,
+      { parent: domainId, count: queryCount, skipAmount: skip }
+    );
+
+    const queriedDomains = queryResult.data.domains;
+    for (const domain of queriedDomains) {
+      domains.push(convertDomainDtoToDomain(domain));
+    }
+
+    /**
+     * We will only get back up to `queryCount` # of domains
+     * So if we get that many there's probably more domains we need
+     * to fetch. If we got back less, we can stop querying
+     */
+    if (queriedDomains.length < queryCount) {
+      break;
+    }
+    skip += queriedDomains.length;
+  }
+
   return domains;
 };
