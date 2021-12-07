4ad76fd707d4b22247d4fb898b1791c712c56e3a
Matt Miller <matthew.c.anthony.miller@gmail.com>
2021-12-06 18:41:43 -0800

    Made script more graceful on failure

--- src/api/actions/uploadInBackground.ts
+++ src/api/actions/uploadInBackground.ts
@@ -1,14 +1,28 @@
 import { checkBulkUploadJob } from ".";
 import { UploadJobStatus, UrlToIPFS, UrlToJobId } from "../../types";
 import { startBulkUpload } from "./startBulkUpload";
+import * as fs from "fs";
 
 const URLS_PER_CHUNK = 100;
+const ERROR_FILEPATH = "error.json";
+let errorJobs: {
+  start: [{ url: string; error: unknown }];
+  check: [{ url: string; jobId: string; error: unknown }];
+};
+
+let failedJobs: {
+  start: [{ url: string; reason: string }];
+  check: [{ url: string; jobId: string }];
+};
+
+let incompleteJobs: UrlToJobId = {};
 
 export const uploadInBackground = async (
   apiUri: string,
   urls: string[],
   uploadedCallback?: (url: string, ipfsUrl: string) => void
 ): Promise<UrlToIPFS> => {
+  let completedJobs: UrlToIPFS = {};
   const urlBatches: string[][] = chunkArray(urls, URLS_PER_CHUNK);
   const startBulkResponses: UrlToJobId = (
     await Promise.all(
@@ -20,9 +34,7 @@ export const uploadInBackground = async (
     );
     return accumulator;
   });
-
-  let incompleteJobs: UrlToJobId = startBulkResponses;
-  let completedJobs: UrlToIPFS = {};
+  incompleteJobs = startBulkResponses;
 
   let jobIdChunks: string[][] = chunkArray(
     Object.values(incompleteJobs),
@@ -38,13 +50,19 @@ export const uploadInBackground = async (
             ? () => {
                 const jobId = jobStatus[0];
                 const jobInfo = jobStatus[1];
-                completedJobs[jobId] = jobInfo.result.hash;
-                uploadedCallback?.(jobId, jobInfo.result.hash);
+                if (jobInfo.failed) {
+                  failedJobs.check.push({ url: incompleteJobs[jobId], jobId });
+                } else {
+                  completedJobs[jobId] = jobInfo.result.hash;
+                  uploadedCallback?.(jobId, jobInfo.result.hash);
+                }
                 delete incompleteJobs[jobId];
               }
             : () => {}
         );
       });
+
+  fs.writeFileSync(ERROR_FILEPATH, JSON.stringify(errorJobs));
 
   return completedJobs;
 };
@@ -53,11 +71,11 @@ const tryStartBulkUpload = async (
   apiUri: string,
   urls: string[]
 ): Promise<UrlToJobId> => {
-  let uploadResponse: UrlToJobId;
+  let uploadResponse: UrlToJobId = {};
   try {
     uploadResponse = await startBulkUpload(apiUri, urls);
   } catch (e) {
-    throw Error(`Bulk upload failed: ${e}`);
+    urls.forEach((url) => errorJobs.start.push({ url: url, error: e }));
   }
   return uploadResponse;
 };
@@ -66,11 +84,17 @@ const tryCheckBulkUploadJob = async (
   apiUri: string,
   jobIds: string[]
 ): Promise<UploadJobStatus> => {
-  let checkResponse: UploadJobStatus;
+  let checkResponse: UploadJobStatus = {};
   try {
     checkResponse = await checkBulkUploadJob(apiUri, jobIds);
   } catch (e) {
-    throw Error(`Check upload status failed: ${e}`);
+    jobIds.forEach((jobId) =>
+      errorJobs.check.push({
+        url: incompleteJobs[jobId] ?? "unknown",
+        jobId,
+        error: e,
+      })
+    );
   }
 
   return checkResponse;
