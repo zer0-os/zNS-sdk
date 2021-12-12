a0a08cf53f76c3e88967c93f3a14c5b9bc0f6a21
James Earle <james@zero.tech>
2021-12-12 16:53:59 -0500

    changes to requesting network name, not from the web3 provider as that may be empty until a valid connection is made but still want to be able to configure

--- src/configuration/configuration.ts
+++ src/configuration/configuration.ts
@@ -14,56 +14,27 @@ export const zAuctionConfiguration = (
   let defaultZAuctionAddress;
   let defaultTokenContract;
 
-  if (network === "mainnet") {
+  if (network === "mainnet" || network === "homestead") {
     defaultApiUri = "https://mainnet.zauction.api.zero.tech/api";
-    defaultSubgraphUri = "https://api.thegraph.com/subgraphs/name/zer0-os/zauction";
+    defaultSubgraphUri =
+      "https://api.thegraph.com/subgraphs/name/zer0-os/zauction";
     defaultZAuctionAddress = "0x05cBD37cA528B7ea50800aA80ddD0F9F30C952F0";
     defaultTokenContract = "0xc2e9678A71e50E5AEd036e00e9c5caeb1aC5987D";
-  } else if (network === "kovan") {
+  } else if (web3Provider.network.name === "kovan") {
     defaultApiUri = "https://zauction-kovan-api.herokuapp.com/api";
-    defaultSubgraphUri = "https://api.thegraph.com/subgraphs/name/zer0-os/zauction-kovan";
+    defaultSubgraphUri =
+      "https://api.thegraph.com/subgraphs/name/zer0-os/zauction-kovan";
     defaultZAuctionAddress = "0x18A804a028aAf1F30082E91d2947734961Dd7f89";
     defaultTokenContract = "0xC613fCc3f81cC2888C5Cccc1620212420FFe4931";
-  } else {
-    throw Error(`Network ${web3Provider.network.name} is not supported`);
-  }
+} else {
+  throw Error(`Network ${network} is not supported`);
+}
 
-  return {
-    web3Provider: web3Provider,
-    apiUri: defaultApiUri,
-    subgraphUri: defaultSubgraphUri,
-    zAuctionAddress: defaultZAuctionAddress,
-    tokenContract: defaultTokenContract,
-  } as zAuction.Config;
-
-  // if (!web3Provider)
-  //   throw Error("Invalid provider given");
-
-  // TODO place back the checks, removing temporarily for dApp team
-  // if (
-  //   web3Provider.network.name == "mainnet" ||
-  //   web3Provider.network.name == "homestead"
-  // ) {
-  //   defaultApiUri = "https://mainnet.zauction.api.zero.tech/api";
-  //   defaultSubgraphUri =
-  //     "https://api.thegraph.com/subgraphs/name/zer0-os/zauction";
-  //   defaultZAuctionAddress = "0x05cBD37cA528B7ea50800aA80ddD0F9F30C952F0";
-  //   defaultTokenContract = "0xc2e9678A71e50E5AEd036e00e9c5caeb1aC5987D";
-  // } else if (web3Provider.network.name === "kovan") {
-  //   defaultApiUri = "https://zauction-kovan-api.herokuapp.com/api";
-  //   defaultSubgraphUri =
-  //     "https://api.thegraph.com/subgraphs/name/zer0-os/zauction-kovan";
-  //   defaultZAuctionAddress = "0x18A804a028aAf1F30082E91d2947734961Dd7f89";
-  //   defaultTokenContract = "0xC613fCc3f81cC2888C5Cccc1620212420FFe4931";
-  // } else {
-  //   throw Error(`Network ${web3Provider.network.name} is not supported`);
-  // }
-
-  // return {
-  //   web3Provider: web3Provider,
-  //   apiUri: apiUri ?? defaultApiUri,
-  //   subgraphUri: subgraphUri ?? defaultSubgraphUri,
-  //   zAuctionAddress: zAuctionAddress ?? defaultZAuctionAddress,
-  //   tokenContract: tokenContract ?? defaultTokenContract,
-  // } as zAuction.Config;
+return {
+  web3Provider: web3Provider,
+  apiUri: apiUri ?? defaultApiUri,
+  subgraphUri: subgraphUri ?? defaultSubgraphUri,
+  zAuctionAddress: zAuctionAddress ?? defaultZAuctionAddress,
+  tokenContract: tokenContract ?? defaultTokenContract,
+} as zAuction.Config;
 };
