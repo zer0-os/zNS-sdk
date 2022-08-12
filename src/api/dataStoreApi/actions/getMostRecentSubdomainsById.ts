b8288635bfd3fd8a54c2c21902b05d6a515f752c
Ryan Cross <ryan.cross.home@gmail.com>
2022-08-12 16:30:03 -0500

    commit files

---
+++ src/api/dataStoreApi/actions/getMostRecentSubdomainsById.ts
@@ -0,0 +1,55 @@
+import { Domain, Maybe } from "../../../types";
+import { DomainCollection } from "../types";
+import { makeApiCall } from "../../helpers";
+import { ethers } from "ethers";
+import { getLogger } from "../../../utilities";
+
+const logger = getLogger().withTag("datastore:actions:getRecentSubdomainsById");
+const MAX_RECORDS = 5000;
+
+export const getMostRecentSubdomainsById = async (
+  apiUri: string,
+  tokenId: string,
+  limit = 100,
+  skip = 0,
+): Promise<Domain[]> => {
+  let response: Maybe<DomainCollection>;
+  if (limit >= MAX_RECORDS) {
+    throw new Error(
+      `Please request no more than ${MAX_RECORDS} records at a time.`
+    );
+  }
+
+  try {
+    logger.trace(
+      `Querying for ${limit} recent subdomains of ${tokenId} starting at indexId ${skip}`
+    );
+
+    response = await makeApiCall<DomainCollection>(
+      `${apiUri}v1/domains/subdomains/deep/${tokenId}?sortDirection=-1&sort=created&skip=${skip}&limit=${limit}`,
+      "GET"
+    );
+  } catch (e) {
+    throw Error(`Failed to get recently created domains for ${tokenId}: ${e}`);
+  }
+
+  // Map from DataStoreDomain -> Domain for downstream consistency
+  const domains: Domain[] = response.results.map((d) => {
+    const domain: Domain = {
+      id: d.domainId,
+      name: d.name,
+      parentId: d.parent,
+      owner: d.owner.value,
+      minter: d.minter,
+      metadataUri: d.metadataUri.value,
+      isLocked: d.locked ? d.locked.value : false,
+      lockedBy: d.lockedBy ? d.lockedBy.value : ethers.constants.AddressZero,
+      contract: d.registrar,
+      isRoot: d.isRoot,
+      created: d.created
+    };
+    return domain;
+  });
+
+  return domains;
+};
