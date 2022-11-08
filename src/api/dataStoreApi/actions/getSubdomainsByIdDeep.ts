c79a6db0e01b116519eef80970ac4443071f271a
Ryan Cross <ryan.cross.home@gmail.com>
2022-11-08 11:56:23 -0600

    add new file

---
+++ src/api/dataStoreApi/actions/getSubdomainsByIdDeep.ts
@@ -0,0 +1,47 @@
+import { Domain, Maybe } from "../../../types";
+import { DomainCollection, DomainSortOptions } from "../types";
+import { makeApiCall } from "../../helpers";
+import { datastoreDomainToDomain } from "../helpers/datastoreDomainToDomain";
+import { desiredSortToQueryParams } from "../helpers/desiredSortToQueryParams";
+
+/**
+ * Get's deeply nested subdomains (children of children and so forth) for a given domain id.
+ */
+export const getSubdomainsByIdDeep = async (
+  apiUri: string,
+  tokenId: string,
+  limit = 100,
+  skip = 0,
+  sort?: DomainSortOptions
+): Promise<Domain[]> => {
+  let response: Maybe<DomainCollection>;
+  let sortBy = ""
+  let sortDirections = ""
+
+  if (sort) {
+    const sortQueryParameterStrings = desiredSortToQueryParams(sort);
+    sortBy = sortQueryParameterStrings.sortQueryString,
+    sortDirections = sortQueryParameterStrings.sortOrderQueryString
+  }
+  
+  try {
+    let requestUri = `${apiUri}v1/domains/subdomains/deep/${tokenId}?skip=${skip}&limit=${limit}`;
+    if (sortBy && sortDirections) {
+      requestUri += `&${sortBy}&${sortDirections}`;
+    }
+
+    response = await makeApiCall<DomainCollection>(
+      requestUri,
+      "GET"
+    );
+  } catch (e) {
+    throw Error(`Failed to get subdomains for ${tokenId}: ${e}`);
+  }
+
+ // Map from DataStoreDomain -> Domain for downstream consistency
+ const domains: Domain[] = response.results.map((d) => {
+  return datastoreDomainToDomain(d);
+});
+
+  return domains;
+};
